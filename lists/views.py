7339982dc5804e200b822500074f8b83305615db
Vagner Moreira da Silva <vagnermsnet501@gmail.com>
2026-09-09 21:44:49 -0300

    Novo Projeto: Atualizando o Template

--- lists/views.py
+++ lists/views.py
@@ -19,3 +19,7 @@ def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
+
+def view_list(request, list_id):
+    my_list = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': my_list})
